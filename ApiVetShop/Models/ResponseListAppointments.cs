f010b11c6fb000d09784bab42e2cb3ef3b1d79e5
LAVS21 <128123474+LAVS21@users.noreply.github.com>
2023-11-14 19:22:08 -0600

    Ya estan todos los responses

---
+++ ApiVetShop/Models/ResponseListAppointments.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiVetShop.Models
+{
+    public class ResponseListAppointments
+    {
+        public List<Appoiments> Appointmets { get; set; } = new List<Appoiments>();
+        public ResponseModel Errors { get; set; } = new ResponseModel();
+    }
+}
---
+++ ApiVetShop/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiVetShop.Models
+{
+    public class ResponseModel
+    {
+        public int errorcode { get; set; }
+        public string? errormsg { get; set; }
+    }
+}
