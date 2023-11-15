bd810df61522d3f88f973471fd8e2db6a2db7189
LAVS21 <128123474+LAVS21@users.noreply.github.com>
2023-11-14 18:42:33 -0600

    adasdad

--- ApiVetShop/Models/Appoiments.cs
+++ ApiVetShop/Models/Appoiments.cs
@@ -2,17 +2,17 @@
 {
     public class Appoiments
     {
-        public int id { get; set; }
-        public string userName { get; set; }
-        public string CustomerName { get; set; }
-        public string unitName { get; set; }
-        public string? siteName { get; set; }
-        public string? asistantName { get; set; }
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? CustomerName { get; set; }
+        public string? UnitName { get; set; }
+        public string? SiteName { get; set; }
+        public string? AsistantName { get; set; }
 
-        public string adress { get; set; }
-        public string canton { get; set; }
-        public string province { get; set; }
-        public string? description { get; set; }
-        public string state { get; set; }
+        public string? Adress { get; set; }
+        public string? Canton { get; set; }
+        public string? Province { get; set; }
+        public string? Description { get; set; }
+        public string? State { get; set; }
     }
 }
