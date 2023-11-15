5e8f0bc6aa9acfcb0cfc2860fd39911a2871ccc7
LAVS21 <128123474+LAVS21@users.noreply.github.com>
2023-11-14 20:11:03 -0600

    Ya estan los IRepositories

---
+++ ApiVetShop/IRepository/IAppointmetsRepository.cs
@@ -0,0 +1,23 @@
+﻿using ApiVetShop.Models;
+using System.Data.SqlTypes;
+
+namespace ApiVetShop.IRepository
+{
+    public interface IAppointmetsRepository
+    {
+        public Task<IEnumerable<Appoiments>> ListAppointmets();
+        public Task<Appoiments> GetAppointmest(int id);
+        public Task<Appoiments> UpdateAppointment(int id, 
+                                                  string NewUserName, 
+                                                  string NewCustomerName, 
+                                                  string NewUnitName, 
+                                                  string NewSiteName,
+                                                  string NewAsistantName,
+                                                  DateTime NewDateTime,
+                                                  string NewAdress,
+                                                  string NewCanton,
+                                                  string NewProvince,
+                                                  string NewDescription,
+                                                  string NewState);
+    }
+}
