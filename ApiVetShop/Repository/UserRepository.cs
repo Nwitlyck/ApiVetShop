e0ae7355ea5e7291b6aae81df2bfb38a455703f2
Nwitlyck <davidsegura118@hotmail.com>
2023-11-20 18:31:18 -0600

    Last Min Change

--- ApiVetShop/Repository/UserRepository.cs
+++ ApiVetShop/Repository/UserRepository.cs
@@ -10,11 +10,9 @@ namespace ApiVetShop.Repository
     public class UserRepository : IUsersRepository
     {
         private readonly IDapperContext _context;
-        private readonly EncrypAPICall _encrypAPICall;
-        public UserRepository(IDapperContext context, EncrypAPICall encrypAPICall)
+        public UserRepository(IDapperContext context)
         {
             _context = context;
-            _encrypAPICall = encrypAPICall;
         }
         public async Task<Users> SelectUser(string email)
         {
@@ -38,10 +36,9 @@ namespace ApiVetShop.Repository
             try
             {
                 var param = new DynamicParameters();
-                var encrypted = await _encrypAPICall.GetEncrypt(password);
 
                 param.Add("@vEmail", email, DbType.String, ParameterDirection.Input);
-                param.Add("@vPassworld", encrypted.encryp, DbType.String, ParameterDirection.Input);
+                param.Add("@vPassworld", password, DbType.String, ParameterDirection.Input);
                 using (var conn = _context.CrearConexion())
                 {
                     return await conn.QuerySingleOrDefaultAsync<Boolean>("[vetShop].[dbo].[verify_user]", param, commandType: CommandType.StoredProcedure);
